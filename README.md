f7eb88a3ad8c83cdfa6c6e7eb483e2acbff062e5
Ricardo Palomares <rpmdisguise-java@yahoo.es>
2022-09-28 18:47:11 +0200

    Add Practice 2 scrapbook directory

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # practicasSE
+
+Proyecto con las prácticas de Sistemas Empotrados (Grado en Ingeniería Informática UCM)
+
+Los directorios prX_scrapbook contienen retazos que pueden ser útiles para la práctica prX correspondiente
---
+++ pr2_scrapbook/practica2_a.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <xstatus.h>
+#include "xparameters.h"
+#include "xil_cache.h"
+#include "uartlite_header.h"
+#include "xbasic_types.h"
+#include "xgpio.h"
+/*#include "gpio_header.h"
+*/
+#define LEDs_HW_BASEaddress (0x81420000)
+#define XPAR_RS232_UART_1_BASEADDR 0x84000000
+
+
+int getSingleDigitNumber (){
+	Xuint8 byte;
+	Xuint8 uartBuffer[16];
+	Xboolean validNumber;
+	int digitIndex;
+	int digit;
+	
+	byte = 0x00;
+	digit = 0;
+	validNumber = XFALSE;
+
+	// Get bytes from uart until a digit is entered
+	while(validNumber == XFALSE){
+		byte = XUartLite_RecvByte(XPAR_RS232_UART_1_BASEADDR);
+        // Only needed if byte must be resent through UART to remote terminal
+		// XUartLite_SendByte(XPAR_RS232_UART_1_BASEADDR,byte);
+
+		// Check if byte is an ASCII digit
+		if((byte >> 4) == 0x03){
+            // Put in digit the decimal value of the ASCII char
+            // (ie.: ASCII "0" --> 0x30 --> digit = 0; ASCII "7" --> 0x37 --> digit = 7;)
+			digit = (byte & 0x0F);
+            validNumber = XTRUE
+		}
+	}
+    return digit;
+}
+
+
+int lightLEDs(
+	XGpio Gpio_LEDs; /* The driver instance for GPIO Device configured as Salida */
+	u32 DataRead;
+	Xil_ICacheEnable();
+    Xil_DCacheEnable();
+
+	// Configuración de la GPIO para los LEDs de la placa extendida
+	XGpio_Initialize(&Gpio_LEDs, XPAR_LEDS_DEVICE_ID); /*Obtiene el puntero a la estructura */
+	XGpio_SetDataDirection(&Gpio_LEDs, 1, 0x0); /*Coloca la dirección de salida */
+
+    // FIXME: completar código
+}

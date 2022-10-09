f45b502cebc615b8dd65f8be081e754b5edcd5d5
Ricardo Palomares Martínez <rpmdisguise-java@yahoo.es>
2022-10-09 22:59:25 +0200

    Practice 2 reorganization and fixing of parts a and b

---
+++ practica2/SDK/practica2_sdk/Debug/src/practica2_b.d
@@ -0,0 +1,61 @@
+src/practica2_b.d: ../src/practica2_b.c \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xstatus.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xbasic_types.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xparameters.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xil_cache.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/mb_interface.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xil_types.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xparameters.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xuartlite_i.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xuartlite.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xil_assert.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xstatus.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xuartlite_l.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xil_io.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xbasic_types.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xgpio.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xgpio_l.h \
+ ../src/gpio_header.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xil_types.h \
+ ../../peripheral_tests_bsp_1/microblaze_0/include/xil_assert.h \
+ ../src/practica2.h
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xstatus.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xbasic_types.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xparameters.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xil_cache.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/mb_interface.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xil_types.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xparameters.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xuartlite_i.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xuartlite.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xil_assert.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xstatus.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xuartlite_l.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xil_io.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xbasic_types.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xgpio.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xgpio_l.h:
+
+../src/gpio_header.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xil_types.h:
+
+../../peripheral_tests_bsp_1/microblaze_0/include/xil_assert.h:
+
+../src/practica2.h:

baf643ab003f97dcc082f89c05d29537ab2025f4
Ricardo Palomares Martínez <rpmdisguise-java@yahoo.es>
2022-11-13 14:38:13 +0100

    Practice 4 changes made by build system

--- practica4/SDK/practica4_sdk/Debug/src/testperiph.d
+++ practica4/SDK/practica4_sdk/Debug/src/testperiph.d
@@ -5,19 +5,18 @@ src/testperiph.d: ../src/testperiph.c \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xil_types.h \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xparameters.h \
- ../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h \
- ../../peripheral_tests_bsp_0/microblaze_0/include/xbram.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xuartlite_i.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xuartlite.h \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xil_assert.h \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h \
- ../../peripheral_tests_bsp_0/microblaze_0/include/xbram_hw.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xuartlite_l.h \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xil_io.h \
- ../src/bram_header.h \
- ../../peripheral_tests_bsp_0/microblaze_0/include/xil_types.h \
- ../../peripheral_tests_bsp_0/microblaze_0/include/xil_assert.h \
- ../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xgpio.h \
  ../../peripheral_tests_bsp_0/microblaze_0/include/xgpio_l.h \
- ../src/gpio_header.h
+ ../src/nrvgap.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h \
+ ../../peripheral_tests_bsp_0/microblaze_0/include/xil_io.h
 
 ../../peripheral_tests_bsp_0/microblaze_0/include/xparameters.h:
 
@@ -31,28 +30,26 @@ src/testperiph.d: ../src/testperiph.c \
 
 ../../peripheral_tests_bsp_0/microblaze_0/include/xparameters.h:
 
-../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h:
+../../peripheral_tests_bsp_0/microblaze_0/include/xuartlite_i.h:
 
-../../peripheral_tests_bsp_0/microblaze_0/include/xbram.h:
+../../peripheral_tests_bsp_0/microblaze_0/include/xuartlite.h:
 
 ../../peripheral_tests_bsp_0/microblaze_0/include/xil_assert.h:
 
 ../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h:
 
-../../peripheral_tests_bsp_0/microblaze_0/include/xbram_hw.h:
+../../peripheral_tests_bsp_0/microblaze_0/include/xuartlite_l.h:
 
 ../../peripheral_tests_bsp_0/microblaze_0/include/xil_io.h:
 
-../src/bram_header.h:
-
-../../peripheral_tests_bsp_0/microblaze_0/include/xil_types.h:
-
-../../peripheral_tests_bsp_0/microblaze_0/include/xil_assert.h:
-
-../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h:
+../../peripheral_tests_bsp_0/microblaze_0/include/xbasic_types.h:
 
 ../../peripheral_tests_bsp_0/microblaze_0/include/xgpio.h:
 
 ../../peripheral_tests_bsp_0/microblaze_0/include/xgpio_l.h:
 
-../src/gpio_header.h:
+../src/nrvgap.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xstatus.h:
+
+../../peripheral_tests_bsp_0/microblaze_0/include/xil_io.h:
